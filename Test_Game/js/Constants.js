51488d5883b024cabea2cedb29538851e4bc02ca
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-04 20:43:26 -0400

    Created .js to hold and organize all constants
    (May not be a good idea, to be seen)

---
+++ Test_Game/js/Constants.js
@@ -0,0 +1,36 @@
+////////////////////////////
+////INVENTORY CONSTANTS////
+//////////////////////////
+
+const weapons = document.getElementById("Weapons");
+const armors = document.getElementById("Armors");
+
+const current_weapon_image = document.getElementById("Weapon_Image");
+const current_armor_image = document.getElementById("Armor_Image");
+
+const equipped_weapon = document.getElementById("Equipped_Weapon");
+const equipped_armor = document.getElementById("Equipped_Armor");
+
+
+
+
+////////////////////////
+////STORE CONSTANTS////
+//////////////////////
+
+const purchase_stick_sword_button_press  = document.getElementById("Purchase_Stick_Sword_Button");
+const purchase_wooden_sword_button_press = document.getElementById("Purchase_Wooden_Sword_Button");
+const purchase_copper_sword_button_press = document.getElementById("Purchase_Copper_Sword_Button");
+
+const purchase_bark_chest_button_press   = document.getElementById("Purchase_Bark_Chest_Button");
+const purchase_wooden_chest_button_press = document.getElementById("Purchase_Wooden_Chest_Button");
+const purchase_copper_chest_button_press = document.getElementById("Purchase_Copper_Chest_Button");
+
+const stick_sword   = document.getElementById("Stick_Sword");
+const wooden_sword  = document.getElementById("Wooden_Sword");
+const copper_sword  = document.getElementById("Copper_Sword");
+
+
+const bark_chest    = document.getElementById("Bark_Chest");
+const wooden_chest  = document.getElementById("Wooden_Chest");
+const copper_chest  = document.getElementById("Copper_Chest");
