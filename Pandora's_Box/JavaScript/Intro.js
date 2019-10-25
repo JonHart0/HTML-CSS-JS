a788c85084678c925e18d0e1703f39adff1b8ad6
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-25 13:14:58 -0400

    Game doesnt start until button press now

---
+++ Pandora's_Box/JavaScript/Intro.js
@@ -0,0 +1,8 @@
+function Start() {
+title.style.display = "block"
+option_panel.style.display = "inline-block"
+health_panel.style.display = "block"
+currency_panel.style.display = "inline-block"
+stat_panel .style.display = "inline-block"
+start_button.style.display = "none"
+}
