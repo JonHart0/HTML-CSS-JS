e22215d894bf2225a4c8cf65c011a5bba9f21bd3
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-25 13:13:19 -0400

    changed player to an object, same as enemies

--- Pandora's_Box/JavaScript/Player.js
+++ Pandora's_Box/JavaScript/Player.js
@@ -2,9 +2,9 @@
 ////Stats////
 ////////////
 
-var player_HP_base  = 10;
-var player_ATK_base  = 1;
-var player_DEF_base = 1;
+//####NEED TO FIX, Make current values initialize from base values####////
+var player = {Base_HP: 10, Base_ATK: 1, Base_DEF:1, Current_HP: 10, Current_ATK: 1, Current_DEF: 1}
+
 
 //Used to remove the item equipped from the equipped arrays
 var last_armor_equipped  = 0;
@@ -16,38 +16,36 @@ var equipped_armor_array  = [1,0,0,0]
 var items_DEF  = 0;
 var items_ATK = 0;
 
-var player_HP = player_HP_base;
-var player_ATK = player_ATK_base;
-var player_DEF = player_DEF_base;
 
 
-const player_health_bar = document.getElementById("Dis_Player_Health_Bar")
 
 function Player_Health_Change() {
-  if (player_HP == 0) {
+  if (player.Current_HP <= 0) {
     location.reload();
   }
-  if (player_HP >= player_HP_base) {
+  if (player.Current_HP >= player.Base_HP) {
     console.log("Full Health!")
-    player_HP = player_HP_base;
+    player.Current_HP = player.Base_HP;
   }
-  player_health_bar.style.width = ((player_HP / player_HP_base) *100) + "%";
-
+  player_health_bar.style.width = ((player.Current_HP / player.Base_HP) *100) + "%";
+  Player_HP_Text.innerHTML = `HP: ${player.Current_HP} / ${player.Base_HP}`
 }
 
 
 
 
 function Player_Update() {
-  player_HP_base   = 10;
-  player_ATK_base  = 1;
-  player_DEF_base  = 1;
+  player.Base_HP  = 10;
+  player.Base_ATK  = 1;
+  player.Base_DEF  = 1;
 
 
   items_DEF   = Armor_List[equipped_armor.selectedIndex].DEF + Weapon_List[equipped_weapon.selectedIndex].DEF;
   items_ATK   = Armor_List[equipped_armor.selectedIndex].ATK + Weapon_List[equipped_weapon.selectedIndex].ATK;
 
-  player_ATK = player_ATK_base + items_ATK;
-  player_DEF = player_DEF_base + items_DEF;
+  player.Current_ATK = player.Base_ATK + items_ATK;
+  player.Current_DEF = player.Base_DEF + items_DEF;
 
+  Dis_Player_ATK.innerHTML = `ATK: ${player.Current_ATK}`;
+  Dis_Player_DEF.innerHTML = `DEF:   ${player.Current_DEF}`;
 }
