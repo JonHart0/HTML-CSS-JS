c4ca3f1e095141180853ff4196eb9ef38d15407b
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-25 13:16:25 -0400

    combat clarity changes

--- Pandora's_Box/JavaScript/Combat.js
+++ Pandora's_Box/JavaScript/Combat.js
@@ -6,11 +6,11 @@
 //copies selected enemies stats into a holding varaible 'current_Enemy'
 function Search() {
   current_Enemy = Object.assign({}, rat_Enemy)
-  current_Enemy.Current_HP = current_Enemy.Base_HP
+  current_Enemy.Current_HP = current_Enemy.HP
   Enemy_Name.innerHTML = current_Enemy.Name;
   Enemy_Health_Panel.style.display = "block";
   enemy_health_bar.style.width = 100 + "%";
-  Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.Current_HP} / ${current_Enemy.Base_HP}`
+  Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.Current_HP} / ${current_Enemy.HP}`
   Find_Enemy_Button.style.display = "none";
   console.log(current_Enemy)
 }
@@ -24,16 +24,22 @@ function Search() {
 //If target has 0 or less HP reveal search button and update attack visual
 function Attack() {
   if (current_Enemy.Current_HP > 0) {
-    current_Enemy.Current_HP -= (player.Current_ATK - current_Enemy.Def)
-    player.Current_HP -= (current_Enemy.ATK - player.Current_DEF)
-    enemy_health_bar.style.width = ((current_Enemy.Current_HP / current_Enemy.Base_HP) *100) + "%";
-    Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.Current_HP} / ${current_Enemy.Base_HP}` }
-    player_health_bar.style.width = ((player.Current_HP / player.Base_HP) *100) + "%";
-    Player_HP_Text.innerHTML = `HP: ${player.Current_HP} / ${player.Base_HP}`
+
+    if ((player.Current_ATK - current_Enemy.DEF) <= 0) {current_Enemy.Current_HP -= 0}
+    else {current_Enemy.Current_HP -= (player.Current_ATK - current_Enemy.DEF);}
+
+    if ((current_Enemy.ATK - player.Current_DEF) <= 0) {player.Current_HP -= 0}
+    else {player.Current_HP -= (current_Enemy.ATK - player.Current_DEF);}
+
+    enemy_health_bar.style.width = ((current_Enemy.Current_HP / current_Enemy.HP) *100) + "%";
+    Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.Current_HP} / ${current_Enemy.HP}`
+
+    Player_Health_Change() }
 
   if (current_Enemy.Current_HP <= 0) {
     Find_Enemy_Button.style.display = "inline-block"
     Enemy_HP_Text.innerHTML = "---Dead---"
     Combat_Message.innerHTML = "Target Dead"
+    enemy_health_bar.style.width = 0;
     window.setTimeout(function(){Combat_Message.innerHTML = ''}, 1000); }
 }
