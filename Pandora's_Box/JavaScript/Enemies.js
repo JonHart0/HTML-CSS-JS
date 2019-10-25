6a28017ec811875a0324a5befe70a7b82336e2ac
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-25 13:12:07 -0400

    New JS file containing enemies and their stats

---
+++ Pandora's_Box/JavaScript/Enemies.js
@@ -0,0 +1,7 @@
+var current_Enemy = {Name: "None", HP: 0, ATK: 0,  DEF:0};
+
+const punching_bag_Enemy = {Name: "Punching Bag", HP: 10, ATK: 0,  DEF:0}
+
+const rat_Enemy = {Name: "Rat", HP: 4, ATK: 2,  DEF:0}
+const wolf_Enemy = {Name: "Wolf", HP: 15, ATK: 4,  DEF:2}
+const bear_Enemy = {Name: "Bear", HP: 25, ATK: 6,  DEF:5}
