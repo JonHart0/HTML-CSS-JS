3c61e303ad9cebb56b77029004e6b827603d9110
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-30 15:20:17 -0400

    Rest and buy potion functions

---
+++ Pandora's_Box/JavaScript/Town.js
@@ -0,0 +1,16 @@
+function Rest() {
+  if (total_copper >= 25 && player.HP < player.Max_HP) {
+    Spend_Currency(25)
+    player.HP += 5;
+    if (player.HP > player.Max_HP) {player.HP = player.Max_HP}
+    Player_Update()
+  }
+}
+
+function Buy_Potion() {
+  if (total_copper >= 50 && player.Potions < player.Max_Potions) {
+    Spend_Currency(50)
+    player.Potions += 1
+    Player_Update()
+  }
+}
