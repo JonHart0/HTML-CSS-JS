b260eb5b6999faf3fd51d6b37de7d06edcbd2028
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-30 15:17:56 -0400

    Added cooldowns on actions, and debug mode to remove timer for testing

--- Pandora's_Box/JavaScript/Combat.js
+++ Pandora's_Box/JavaScript/Combat.js
@@ -4,17 +4,20 @@
 
 
 function Search() {
-encounter_index = Encounters();
+  if (timeroff == false) {CoolDown(Search_Button,3)}
+  encounter_index = Encounters();
   switch (encounter_index){
     case '0':
+    console.log("TEST")
     enemy = Weighted_Zone_Spawn(zone_one_enemies);
     Enemy_Found(enemy)
       break;
 
     case '1':
-    Combat_Message.innerHTML = "Found 10 Copper!"
+    found_reward = Math.floor(Math.random()*20)
+    copper += found_reward;
+    Combat_Message.innerHTML = `Found ${found_reward} Copper!`
     window.setTimeout(function(){Combat_Message.innerHTML = ''}, 2000);
-    copper += 10;
       break;
 
     case '2':
@@ -62,30 +65,34 @@ function Player_ATK() {
   if ((player.ATK - current_Enemy.DEF) <= 0) {current_Enemy.HP -= 0}
   else {current_Enemy.HP -= (player.ATK - current_Enemy.DEF);}
 }
+
 //If target has more than 0 HP subtract HP from enemy and update visuals
 //If target has 0 or less HP reveal search button and update attack visual
 function Attack() {
   if (current_Enemy.HP > 0) {
     Player_ATK()
     Enemy_ATK()
-
+    Enemy_Health_Bar.style.display = "block";
     Enemy_Health_Bar.style.width = ((current_Enemy.HP / current_Enemy.Max_HP) *100) + "%";
     Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.HP} / ${current_Enemy.Max_HP}`
 
-    Player_Health_Change() }
+    Player_Update() }
 
   if (current_Enemy.HP <= 0) {
     Enemy_Dead()
     Enemy_Reward(enemy)
   }
+  if (timeroff == false) {CoolDown(Attack_Button,1)};
 }
 
 function Enemy_Found(enemy) {
-  player.InCombat = "True";
+  player.InCombat = true;
+  Explore_Button.style.display = "none"
   current_Enemy = Object.assign({}, enemy)
 
   Attack_Button.style.display = "inline-block";
   Run_Button.style.display = "inline-block";
+  Potion_Button.style.display = "inline-block";
 
   Enemy_Name.innerHTML = current_Enemy.Name;
   Enemy_Name.style.display = "block";
@@ -97,7 +104,7 @@ function Enemy_Found(enemy) {
   Run_Button.title = `${(current_Enemy.Esc_Rate)*100}%`
 
   Enemy_HP_Text.innerHTML = `HP: ${current_Enemy.HP} / ${current_Enemy.Max_HP}`
-  Find_Enemy_Button.style.display = "none";
+  Search_Button.style.display = "none";
   Enemy_Name.title = current_Enemy.Description; //Change the hover text on the name of the enemy
 }
 
@@ -109,14 +116,18 @@ function Enemy_Dead() {
 
   Attack_Button.style.display = "none";
   Run_Button.style.display = "none";
+  Potion_Button.style.display = "none";
+  Explore_Button.style.display = "inline-block"
 
-  player.InCombat = "False";
-  Find_Enemy_Button.style.display = "inline-block"
-  window.setTimeout(function(){Combat_Message.innerHTML = ''}, 2000);
+  player.InCombat = false;
+  Search_Button.style.display = "inline-block"
+  window.setTimeout(function(){Combat_Message.innerHTML = ''; Enemy_Health_Panel.style.display = "none";}, 1000);
 }
 
 
 function Enemy_Reward(enemy) {
+  player.EXP += current_Enemy.EXP;
+  Player_Level_Check()
   switch (enemy.Reward_Type) {
     case "Copper":
       copper += enemy.Reward_Amount;
@@ -139,6 +150,52 @@ function Run() {
     Combat_Message.innerHTML = "You got away!"
   } else {
     Enemy_ATK()
-    Player_Health_Change()
+    Player_Update()
+    if (timeroff == false) {CoolDown(Run_Button,2)};
   }
+}
+
+
+
+
+////timer////
+timeroff = false;
+
+function CoolDown(Button,cooldown_time,test = true) {
+  Button.disabled = true;
+
+  if (cooldown_time < 10) {
+    Button.innerHTML = "0" + cooldown_time;
+  } else {
+    Button.innerHTML = cooldown_time;
+  }
+  if (cooldown_time <= 0) {
+    Button.disabled = false;
+    switch (Button) {
+      case Search_Button:
+        Button.innerHTML = "Search"
+        break;
+      case Attack_Button:
+        Button.innerHTML = "Attack"
+        break;
+
+      case Run_Button:
+        Button.innerHTML = "Run"
+        break;
+
+    }
+    return;
+  }
+  cooldown_time -= 1;
+  setTimeout(function(){ CoolDown(Button,cooldown_time,false) }, 1000);
+}
+
+function Potion() {
+
+  if (player.Potions > 0 && player.HP < player.Max_HP) {
+    player.Potions -= 1;
+    player.HP += 5;
+    Player_Update()
+  }
+
 }
