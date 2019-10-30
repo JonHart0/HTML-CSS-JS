5470cb85f32371ba67cab0818baf869f432e5b06
JonHart0 <56044060+JonHart0@users.noreply.github.com>
2019-10-30 15:18:21 -0400

    a mess

--- Pandora's_Box/JavaScript/Intro.js
+++ Pandora's_Box/JavaScript/Intro.js
@@ -7,8 +7,11 @@ Start_Button.style.display = "none"
 Player_Name.innerHTML = "Player";
 
 Inventory_Panel.style.display = "none"
-Store_Panel.style.display = "none"
+Armory_Panel.style.display = "none"
 Explore_Panel.style.display = "none"
 Debug_Panel.style.display = "none"
+Town_Panel.style.display = "none"
+Town_Inn_Panel.style.display = 'none'
+Apothecary_Panel.style.display = "none"
 }
 Start();
